f4a5ecf033019d24f153679e3c6a31d4b2ad697b
libi <843298330@qq.com>
2022-05-26 23:07:20 +0800

    add sal

---
+++ src/main/resources/sql/ddl.sql
@@ -0,0 +1,29 @@
+CREATE TABLE `History10_77A` (
+  `ID` int NOT NULL,
+  `n1` int DEFAULT NULL,
+  `n2` int DEFAULT NULL,
+  `n3` int DEFAULT NULL,
+  `n4` int DEFAULT NULL,
+  `n5` int DEFAULT NULL,
+  `n6` int DEFAULT NULL,
+  `n7` int DEFAULT NULL,
+  `n8` int DEFAULT NULL,
+  `n9` int DEFAULT NULL,
+  `n10` int DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `F27X9_133a` (
+  `ID` int NOT NULL,
+  `s1` int DEFAULT NULL,
+  `s2` int DEFAULT NULL,
+  `s3` int DEFAULT NULL,
+  `s4` int DEFAULT NULL,
+  `s5` int DEFAULT NULL,
+  `s6` int DEFAULT NULL,
+  `s7` int DEFAULT NULL,
+  `s8` int DEFAULT NULL,
+  `s9` int DEFAULT NULL,
+  `s10` int DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
